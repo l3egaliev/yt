7fb89aa239e0c40bc8d5b1387939aa7a0cd392d9
Rocky <tedrocky@gmail.com>
2023-03-21 14:32:27 +0600

    YtYt commited

---
+++ js/main.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function(){
+    let cont = document.querySelector('.cont');
+let btn = document.querySelector('.btn');
+
+function click(){
+    cont.style.backgroundColor = black
+}
+
+btn.addEventListener('click', function(){
+    click()
+})
+click()
+})
